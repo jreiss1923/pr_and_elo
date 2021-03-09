e56b5ad884c4f456fd26d360b42db3d41455d4b3
jreiss1923 <joshua.reiss2@gmail.com>
2021-03-09 18:58:02 -0500

    Created DB-less functionality, edited prints

--- elo_and_pr_no_db.py
+++ elo_and_pr_no_db.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import elo
-import mysql.connector
 from dotenv import load_dotenv
 import os
 import argparse
@@ -15,17 +14,6 @@ load_dotenv()
 
 # uses 32 k factor as the classical standard for chess and for a (imo) good-sized swing in points
 kkr_elo_calc = elo.Elo(k_factor=32, rating_class=float, initial=1500, beta=200)
-
-# if you want to use this for yourself, replace this with your own mysql credentials
-# connect to mysql db locally
-elo_database = mysql.connector.connect(
-    host=os.getenv('HOST'),
-    user=os.getenv('USERNAME_DB'),
-    password=os.getenv('PASSWORD'),
-    database=os.getenv("DB_NAME")
-)
-
-cursor = elo_database.cursor()
 
 # spreadsheet data
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -95,10 +83,11 @@ def update_sheet_data(values, range_code):
 # adds user to the database with a default starting ELO of 1500
 def add_user(username):
     try:
-        query = "INSERT INTO elo (username, elo, pr) VALUES ('" + username + "', 1500, 0)"
-        cursor.execute(query)
 
-        elo_database.commit()
+        elo_data = get_sheet_data(RANGE_NAME_ELO)
+        for user_data in elo_data:
+            if username == user_data[0]:
+                raise ValueError("This username exists in your PR already")
 
         values = [
             [
@@ -141,23 +130,14 @@ def add_match(winner, loser, gw, gl):
             ]
         ]
 
-        query = "INSERT INTO match_history (winner, loser, gw, gl) VALUES ('" + winner + "', '" + loser + "', " + str(
-            gw) + ", " + str(gl) + ")"
-        cursor.execute(query)
-
         append_sheet_data(values, RANGE_NAME_MATCHES)
-
-        query = "SELECT elo FROM elo WHERE username='" + winner + "'"
-        cursor.execute(query)
-        winner_elo = cursor.fetchall()
 
         elo_data = get_sheet_data(RANGE_NAME_ELO)
         winner_elo_str = find_elo("ELO", elo_data, winner)
         loser_elo_str = find_elo("ELO", elo_data, loser)
 
-        query = "SELECT elo FROM elo WHERE username='" + loser + "'"
-        cursor.execute(query)
-        loser_elo = cursor.fetchall()
+        winner_elo = get_sheet_data(winner_elo_str)
+        loser_elo = get_sheet_data(loser_elo_str)
 
         elos = kkr_elo_calc.rate_1vs1(winner_elo[0][0], loser_elo[0][0])
 
@@ -176,14 +156,6 @@ def add_match(winner, loser, gw, gl):
             ]
         ]
 
-        query = "UPDATE elo SET elo=" + str(winner_elo) + " WHERE username='" + winner + "'"
-        cursor.execute(query)
-
-        query = "UPDATE elo SET elo=" + str(loser_elo) + " WHERE username='" + loser + "'"
-        cursor.execute(query)
-
-        elo_database.commit()
-
         update_sheet_data(winner_values, winner_elo_str)
         update_sheet_data(loser_values, loser_elo_str)
 
@@ -197,15 +169,14 @@ def add_match(winner, loser, gw, gl):
 # returns a list of the top x users by ELO
 def list_top_x(top_num):
     try:
-        query = "SELECT username FROM elo ORDER BY elo DESC LIMIT " + str(top_num)
-        cursor.execute(query)
-        top_users = cursor.fetchall()
-        top_users_list = []
+        elo_data = get_sheet_data(RANGE_NAME_ELO)
 
-        for sublist in top_users:
-            list(sublist)
-            for item in sublist:
-                top_users_list.append(item)
+        top_users_list = [None] * top_num
+
+        for user_info in elo_data:
+            top_users_list[int(user_info[2]) - 1] = user_info[0]
+
+        top_users_list = list(filter(None, top_users_list))
 
         return top_users_list
     except Exception as e:
@@ -223,12 +194,6 @@ def update_pr():
     elo_data = get_sheet_data(RANGE_NAME_ELO)
     top_list = list_top_x(1000)
     for user in top_list:
-
-        query = "UPDATE elo SET pr=" + str(top_list.index(user) + 1) + " WHERE username='" + user + "'"
-        cursor.execute(query)
-
-        elo_database.commit()
-
         pr_square = find_elo("PR", elo_data, user)
         values = [
             [
@@ -243,9 +208,6 @@ def update_pr():
 # should use if temporarily banning or suspected cheating/wintrading has occurred
 def reset_user(username):
     try:
-        query = "UPDATE elo SET elo=1500 WHERE username='" + username + "'"
-        cursor.execute(query)
-
         elo_data = get_sheet_data(RANGE_NAME_ELO)
         elo_range = find_elo("ELO", elo_data, username)
 
@@ -256,8 +218,6 @@ def reset_user(username):
         ]
 
         update_sheet_data(values, elo_range)
-
-        elo_database.commit()
 
         update_pr()
 
@@ -270,9 +230,6 @@ def reset_user(username):
 # keeps match history to demonstrate why elo changed for another user
 def delete_user(username):
     try:
-        query = "DELETE FROM elo WHERE username='" + username + "'"
-        cursor.execute(query)
-
         elo_data = get_sheet_data(RANGE_NAME_ELO)
         elo_range = find_elo("row", elo_data, username)
 
@@ -283,8 +240,6 @@ def delete_user(username):
         ]
 
         update_sheet_data(values, elo_range)
-
-        elo_database.commit()
 
         update_pr()
 
@@ -299,7 +254,7 @@ def initialize_parser():
 
     # elo_and_pr {-d | -deleteUser} [username]
     parser.add_argument("-d", "-deleteUser", nargs='?',
-                        help="Deletes a user from the PR", type=str)
+                        help="Deletes a user from the PR,", type=str)
 
     # elo_and_pr {-r | -resetUser} [username]
     parser.add_argument("-r", "-resetUser", nargs='?',
